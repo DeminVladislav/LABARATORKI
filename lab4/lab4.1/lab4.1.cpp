aa6b458c1d41c03008288d3885e73cbdbe0cdec4
Roun1 <vladok_2003@inbox.ru>
2021-09-25 21:56:49 +0300

    Create lab4.1.cpp

---
+++ lab4/lab4.1/lab4.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main() {
+    setlocale(LC_ALL, "rus");
+    using namespace std;
+    int a, b, S, P;
+    cout << "Введите стороны a и b:" << endl;
+    cin >> a >> b;
+    S = a * b;
+    P = 2 * (a + b);
+    cout << "Площадь равна: " << S << endl;
+    cout << "Периметр равен: " << P;
+    return 0;
+}
