d82b27e97c224527c56d0b8970c8e1207e87408c
Roun1 <vladok_2003@inbox.ru>
2021-09-25 13:26:35 +0300

    Create lab41.cpp

---
+++ lab41.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+int main() {
+    using namespace std;
+    int a, b, S, P;
+    cin >> a >> b;
+    S = a * b;
+    P = 2 * (a + b);
+    cout << S << " " << P;
+    return 0;
+}
