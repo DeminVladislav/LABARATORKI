bcf6a570f32d78a53f55b560246ee6619528ff52
Roun1 <vladok_2003@inbox.ru>
2021-09-26 02:24:01 +0300

    Create lab4.5.cpp

---
+++ lab4/lab4.5/lab4.5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+int main() {
+    setlocale(LC_ALL, "rus");
+    using namespace std;
+    double a, b;
+    cout << "Введите числа a и b:" << endl;
+    cin >> a >> b;
+    a = abs(a);
+    b = abs(b);
+    cout << "Сумма (|a| + |b|) равна: " << (a + b) << endl;
+    cout << "Разность (|a| - |b|) равна: " << (a - b) << " Разность (|b| - |a|) равна: " << (b - a) << endl;
+    cout << "Умножение (|a| * |b|) равно: " << (a * b) << endl;
+    cout << "Частное (|a| / |b|) равно: " << (a / b) << " Частное (|b| / |a|) равно: " << (b / a);
+    return 0;
+}
