e848b0e1a879721c64ad077f9542245716a086a9
Roun1 <vladok_2003@inbox.ru>
2021-09-25 21:52:22 +0300

    Create lab4.2.cpp

---
+++ lab4/lab4.2/lab4.2.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+int main() {
+	setlocale(LC_ALL, "rus");
+	using namespace std;
+	int d;
+	cout << "Введите диагональ:" << endl;
+	cin >> d;
+	double L, pi = 3.14;
+	L = pi * d;
+	cout <<"Длина круга равна: " << L;
+}
